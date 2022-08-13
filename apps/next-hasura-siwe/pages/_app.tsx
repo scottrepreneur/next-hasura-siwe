1ccb63f3a4cd5eccc6de939a1f18315079414edc
Scott Herren <me@stwoh.com>
2022-08-13 14:43:31 -0500

    next-auth and siwe

--- apps/next-hasura-siwe/pages/_app.tsx
+++ apps/next-hasura-siwe/pages/_app.tsx
@@ -3,24 +3,30 @@ import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiConfig } from 'wagmi';
+import { SessionProvider } from 'next-auth/react';
 import { wagmiClient, chains } from '../utils/web3';
 
 import '@rainbow-me/rainbowkit/styles.css';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+function CustomApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <>
       <Head>
         <title>Welcome to next-hasura-siwe!</title>
       </Head>
       <ChakraProvider>
-        <WagmiConfig client={wagmiClient}>
-          <RainbowKitProvider chains={chains}>
-            <main className="app">
-              <Component {...pageProps} />
-            </main>
-          </RainbowKitProvider>
-        </WagmiConfig>
+        <SessionProvider session={session}>
+          <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider chains={chains}>
+              <main className='app'>
+                <Component {...pageProps} />
+              </main>
+            </RainbowKitProvider>
+          </WagmiConfig>
+        </SessionProvider>
       </ChakraProvider>
     </>
   );
---
+++ apps/next-hasura-siwe/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,64 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { getCsrfToken } from 'next-auth/react';
+import { SiweMessage } from 'siwe';
+
+export default async function auth(req: any, res: any) {
+  const providers = [
+    CredentialsProvider({
+      name: 'Ethereum',
+      credentials: {
+        message: {
+          label: 'Message',
+          type: 'text',
+          placeholder: '0x0',
+        },
+        signature: {
+          label: 'Signature',
+          type: 'text',
+          placeholder: '0x0',
+        },
+      },
+      async authorize(credentials) {
+        try {
+          const siwe = new SiweMessage(
+            JSON.parse(credentials?.message || '{}')
+          );
+
+          const nextAuthUrl = process.env.NEXTAUTH_URL;
+
+          const nextAuthHost = new URL(nextAuthUrl).host;
+          if (siwe.domain !== nextAuthHost) {
+            return null;
+          }
+
+          if (siwe.nonce !== (await getCsrfToken({ req }))) {
+            return null;
+          }
+
+          await siwe.validate(credentials?.signature || '');
+          return {
+            // id gets set as sub in the token
+            id: siwe.address,
+          };
+        } catch (e) {
+          return null;
+        }
+      },
+    }),
+  ];
+
+  return await NextAuth(req, res, {
+    providers,
+    session: {
+      strategy: 'jwt',
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+      async session({ session, token }) {
+        session.address = token.sub;
+        return session;
+      },
+    },
+  });
+}
